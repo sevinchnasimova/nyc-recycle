c4312e1614931f71e681d7c27c2e642a6dfd5f66
Sevinch Nasimova <sevanasimova2006@gmail.com>
2023-12-04 02:53:54 -0500

    adding home page structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        theme: "#E0F3D4",
+      },
+    },
+  },
+  plugins: [],
+};
